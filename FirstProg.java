1ec3684bc89e4b113782f67344b246a4ff3b85c0
Michael Williams <michaelwilliams@SDMacBookPro2.local>
2016-09-26 12:16:11 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+//This is the first programming example for Learn Java the Hard Way
+
+public class FirstProg {
+	public static void main( String[] args){
+		System.out.println( "I, Michael Andrew Williams, am determined to learn how to code." );
+		System.out.println( "Today's date is Thursday, August 25, 2016." );
+	}
+}
